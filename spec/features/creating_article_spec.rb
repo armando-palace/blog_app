774989286f3468ebf393241e45e9631e8badc391
Jesús Di Bari <iijbari@gmail.com>
2017-05-29 07:12:48 +0000

    Add validations to Article

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Articles" do
-	scenario "A user creatis a new article" do
+	scenario "A user creates a new article" do
 		# For this scenario, you need to go first to a URL
 		visit "/"
 		
@@ -15,5 +15,19 @@ RSpec.feature "Creating Articles" do
 		expect(page).to have_content("Article has been created")
 		# The current path of the page is going to be the articles_path
 		expect(page.current_path).to eq(articles_path)
+	end
+	
+	scenario "A user fails to create a new article" do
+		visit "/"
+		
+		click_link "New Article"
+		
+		fill_in "Title", with: ""
+		fill_in "Body", with: ""
+		click_button "Create Article"
+		
+		expect(page).to have_content("Article has not been created")
+		expect(page).to have_content("Title can't be blank")
+		expect(page).to have_content("Body can't be blank")
 	end
 end
